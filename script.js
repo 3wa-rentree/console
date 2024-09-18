8e774111cc91539fbfbc99942d784766c5e8b73c
3wa <charlotte.varlet@3wa.io>
2024-09-18 16:27:07 +0200

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+// const numberElem = document.getElementById("number");
+// for (let i = 0; i < 7; i++) {
+// for let i = 0; i < 7; i++ {
+//   numberlem.inerText = i;
+//   numberElem.innerText = i;
+// }
